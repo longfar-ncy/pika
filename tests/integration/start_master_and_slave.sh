835dc43b9b8c6bea9216e702f323769fe91eb362
longfar <ningchangyuan@nuaa.edu.cn>
2023-09-09 17:19:53 +0800

    better test script

--- tests/integration/start_master_and_slave.sh
+++ tests/integration/start_master_and_slave.sh
@@ -9,6 +9,10 @@ sed -i '' -e 's|databases : 1|databases : 2|' -e 's|port : 9221|port : 9231|' -e
 ./pika -c ./pika_master.conf
 ./pika -c ./pika_slave.conf
 #ensure both master and slave are ready
-while ! nc -z 127.0.0.1 9231; do
-	sleep 1
+for i in {1..100}; do
+	if lsof -i:9231 > /dev/null; then
+		break
+	else
+		sleep 1
+	fi
 done
